89a27722c42a49967522dc1b6ff4a750c822486a
rsd477 <rds95@drexel.edu>
2022-05-12 17:47:09 -0400

    adding POST

---
+++ public_html/js/main.js
@@ -0,0 +1,24 @@
+function submit(){
+    let brightness = document.getElementById("brightness").value;
+    let fanSpeed = document.getElementById("fanSpeed").value;
+    console.log("brightness is: " + brightness + " fan speed is: " + fanSpeed);
+
+    fetch('/settings', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            brightness: brightness,
+            fan_speed: fanSpeed
+            }),
+        }).then(function(res){
+            if(res.status === 200){
+                console.log("success");
+                window.location.pathname = '/members'
+            } else {
+                console.log("server error");
+            }
+        }).catch((error) => {console.log(error)});
+    }
+
