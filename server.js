e868c10a735560ebf03d790eecda3452e7eda1d8
rsd477 <rds95@drexel.edu>
2022-05-12 17:55:53 -0400

    backend brightness test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require("express");
+let exec = require("child_process");
 let port = 3000;
 let hostname = "localhost"
 let app = express();
@@ -8,6 +9,28 @@ app.use(express.json());
 
 app.get('/', function(req,res) {
 	res.status(200).sendFile(__dirname + "\\public_html\\index.html\\");
+});
+
+app.post("/settings", function (req, res) {
+	let body = req.body;
+
+	if(body.hasOwnProperty("brightness") && body.hasOwnProperty("fan_speed")){
+		exec(`brightness ${body.brightness}`, (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				return;
+			}
+			console.log(`stdout: ${stdout}`);
+		});
+		return res.sendStatus(200);
+	} else {
+		return res.sendStatus(400);
+	}
+
 });
 
 app.listen(port,() => {
